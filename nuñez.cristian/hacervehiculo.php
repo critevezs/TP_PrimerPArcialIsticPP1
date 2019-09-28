ae15713d8afebe7891861d7cc6f461e451acd14b
critevezs <54085883+critevezs@users.noreply.github.com>
2019-09-28 17:20:19 -0300

    Add files via upload
    agregue la hora de ingreso del vehicuo de forma automatica

--- nuñez.cristian/hacervehiculo.php
+++ nuñez.cristian/hacervehiculo.php
@@ -1,8 +1,13 @@
 <?php
-$miObjeto = new stdClass();
-$miObjeto->patente = $_GET['patente'];
+ $miObjeto = new stdClass();
+ $horaVehiculo = time(); 
+ date_default_timezone_set('America/Argentina/Buenos_Aires');
+ $horaVehiculo = date ("H:i", $horaVehiculo);
+ $miObjeto->patente = $_GET['patente'];
+ $miObjeto->horaVehiculo = $horaVehiculo;
+ $archivo = fopen('vehiculos.txt', 'a');
+ fwrite($archivo, json_encode($miObjeto)."\n");
+ fclose($archivo);
+ header("Location: ok.php");
 
-$archivo = fopen('vehiculos.txt', 'a');
-fwrite($archivo, json_encode($miObjeto)."\n");
-fclose($archivo);
 ?>
