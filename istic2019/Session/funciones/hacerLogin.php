4b29ad1ba7c2086d92d45afaa8ec080c57bc8bba
critevezs <54085883+critevezs@users.noreply.github.com>
2019-10-17 03:49:34 -0300

    Add files via upload
    se agrego la carpeta istic2019/session y se fueron agregando archivos de la aplicación del estacionamiento y modificando el código

---
+++ istic2019/Session/funciones/hacerLogin.php
@@ -0,0 +1,55 @@
+<?php
+	session_start();
+
+	$usuarioIngresado = $_GET['usuario'];
+	$claveIngresada = $_GET['contraseña'];
+	
+	$booUsuario = 0;
+	$booPassword = 0;
+
+	if (empty($usuarioIngresado) || empty($claveIngresada)) 
+	{
+		header("Location: ../paginas/login.php?error=camposvacios");
+		exit();
+	}
+	else
+	{
+		$archivo = fopen("../archivos/registro.txt", "r") or die("Imposible arbrir el archivo");
+	
+		while(!feof($archivo)) 
+		{
+			$objeto = json_decode(fgets($archivo));
+			if ($objeto->nombre == $usuarioIngresado) 
+			{	
+				$booUsuario = 1;
+				if ($objeto->contraseña == $claveIngresada)
+				{
+				    
+
+					fclose($archivo);
+					$_SESSION['usuario']=$objeto->nombre;
+					$_SESSION['perfil']=$objeto->perfil;
+
+					header("Location: ../paginas/login.php?exito=signup");
+					exit();
+				}			
+			}
+		 	
+		}	
+		if ($booUsuario == 0) {
+			header("Location: ../paginas/login.php?error=usuarioincorrecto");
+			fclose($archivo);
+			exit();
+		}
+		else 
+	    {
+			header("Location: ../paginas/login.php?error=contraseñaincorrecta");
+			fclose($archivo);
+			exit();
+		}
+
+		fclose($archivo);
+	}	
+	header("Location: ../paginas/login.php");
+	exit();
+?>
---
+++ istic2019/Session/funciones/hacerfacturar.php
@@ -0,0 +1,61 @@
+<?php
+
+$precio=200;
+$patente2 = $_GET['patente'];
+
+
+
+$archivo = fopen('vehiculos.txt', 'r');
+
+while(!feof ($archivo))
+{
+	$objeto = json_decode(fgets($archivo));
+
+
+    if ($objeto->patente == $patente2)
+    {
+    
+        date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+        $horaSalida = mktime();
+
+        $tiempo = $horaSalida - $objeto->horaIngreso;
+
+        $cobrar = ($tiempo / 60 /60) * $precio;
+    
+
+       $objetoFacturado = new stdClass();
+
+       date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+       $objetoFacturado->Vehiculo = $patente2;
+       $objetoFacturado->fechaEntrada = date("d-m-y H:i",$objeto->horaIngreso);
+       $objetoFacturado->fechaSalida = date("d-m-y H:i",$horaSalida);
+       $objetoFacturado->importe = $cobrar;
+    
+       $archivo1 = fopen('facturados.txt', 'a');
+       fwrite($archivo1, json_encode($objetoFacturado)."\n");
+       fclose($archivo1);
+       
+       
+       header("Location: pagar.php? &cobrar=".$cobrar."&ingreso=".$objeto->horaIngreso."&salida=".$horaSalida."&patente=".$patente2);
+       break;
+    
+   }
+
+   else
+   {
+       header("Location: no.php");
+   }
+ 
+}
+
+fclose($archivo);
+
+?>
+
+
+
+
+
+
